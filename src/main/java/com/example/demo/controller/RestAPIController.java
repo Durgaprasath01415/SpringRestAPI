933d61dc39f07849775036b2f2614bb5ea1c11ce
Durgaprasath01415 <sdp8754262827@gmail.com>
2021-07-08 18:41:09 +0530

    ADDED : Different Rest API calls to create hello message app

--- src/main/java/com/example/demo/controller/RestAPIController.java
+++ src/main/java/com/example/demo/controller/RestAPIController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.User;
+
 @RestController
 @RequestMapping("/hello")
 public class RestAPIController {
